5c03f6728d9cbc188cc2f4d83ef64dbc583c7782
jlallawm <joshmanlallawmawma@gmail.com>
2026-05-26 23:52:26 +0530

    If else and code cleanup almost production ready

--- Profiler.cpp
+++ Profiler.cpp
@@ -5,68 +5,98 @@
 
 namespace prf
 {
-    ProfileInput& getProfileInput()
+    static std::map<std::string, std::unique_ptr<BlockProfiler>, std::less<>> &getInstances();
+
+    ProfileInput &getProfileInput()
     {
         static ProfileInput l_input = {
-            [](const std::string& f_str) { std::cout << f_str; },
-            []() -> uint64_t {
+            [](const std::string &f_str)
+            { std::cout << f_str; },
+            []() -> uint64_t
+            {
                 return static_cast<uint64_t>(std::chrono::duration_cast<std::chrono::nanoseconds>(
-                    std::chrono::steady_clock::now().time_since_epoch()
-                ).count());
-            }
-        };
+                                                 std::chrono::steady_clock::now().time_since_epoch())
+                                                 .count());
+            }};
         return l_input;
     }
 
-    static std::map<std::string, std::unique_ptr<BlockProfiler>, std::less<>>& getInstances();
-
-    std::atomic<bool>& BlockProfiler::getProfileEnabled()
+    std::atomic<bool> &BlockProfiler::getProfileEnabled()
     {
         static std::atomic<bool> l_isProfileEnabled{false};
         return l_isProfileEnabled;
     }
 
-    std::mutex& getMapMutex()
-    {
-        static std::mutex l_mapMutex{};
-        return l_mapMutex;
-    }
+    static std::mutex s_mapMutex{};
 
-    std::map<std::string, std::unique_ptr<BlockProfiler>, std::less<>>& getInstances()
+    std::map<std::string, std::unique_ptr<BlockProfiler>, std::less<>> &getInstances()
     {
         static std::map<std::string, std::unique_ptr<BlockProfiler>, std::less<>> l_instances{};
         return l_instances;
     }
 
-    void BlockProfiler::profilerInit(const ProfileInput& f_input)
+    void BlockProfiler::profilerInit(const ProfileInput &f_input)
     {
-        if (f_input.m_logCallback) getProfileInput().m_logCallback = f_input.m_logCallback;
-        if (f_input.m_getCurrentTime) getProfileInput().m_getCurrentTime = f_input.m_getCurrentTime;
-        getProfileInput().m_logCallback("Profiler Status:"+ std::to_string(BlockProfiler::getProfileEnabled().load(std::memory_order_relaxed) ? 1 : 0) + "\n");
+        static std::once_flag l_initFlag;
+        std::call_once(l_initFlag, [&f_input]()
+                       {
+            if (f_input.m_logCallback)
+            {
+                getProfileInput().m_logCallback = f_input.m_logCallback;
+            }
+            if (f_input.m_getCurrentTime)
+            {
+                getProfileInput().m_getCurrentTime = f_input.m_getCurrentTime;
+            }
+            getProfileInput().m_logCallback("Profiler Status:"+ std::to_string(BlockProfiler::getProfileEnabled().load(std::memory_order_relaxed) ? 1 : 0) + "\n"); });
     }
 
-    void BlockProfiler::print(const Precision f_precision, const std::vector<std::string>& f_blockNames)
+    void BlockProfiler::print(const Precision f_precision, const std::vector<std::string> &f_blockNames)
     {
         double l_divisor = 1.0;
-        const char* l_unit = "ns";
-        if (f_precision == Precision::Micro) { l_divisor = 1000.0; l_unit = "us"; }
-        else if (f_precision == Precision::Milli) { l_divisor = 1000000.0; l_unit = "ms"; }
+        const char *l_unit = "ns";
+        if (f_precision == Precision::Micro)
+        {
+            l_divisor = 1000.0;
+            l_unit = "us";
+        }
+        else if (f_precision == Precision::Milli)
+        {
+            l_divisor = 1000000.0;
+            l_unit = "ms";
+        }
+        else
+        {
+            /*Do Nothing*/
+        }
         printImpl(l_divisor, l_unit, getProfileInput().m_logCallback, f_blockNames);
     }
 
     void BlockProfiler::printAll(const Precision f_precision)
     {
         double l_divisor = 1.0;
-        const char* l_unit = "ns";
-        if (f_precision == Precision::Micro) { l_divisor = 1000.0; l_unit = "us"; }
-        else if (f_precision == Precision::Milli) { l_divisor = 1000000.0; l_unit = "ms"; }
+        const char *l_unit = "ns";
+        if (f_precision == Precision::Micro)
+        {
+            l_divisor = 1000.0;
+            l_unit = "us";
+        }
+        else if (f_precision == Precision::Milli)
+        {
+            l_divisor = 1000000.0;
+            l_unit = "ms";
+        }
+        else
+        {
+            /*Do Nothing*/
+        }
         printImpl(l_divisor, l_unit, getProfileInput().m_logCallback, {});
     }
 
-    void BlockProfiler::printImpl(const double f_divisor, const char* const f_unit, const std::function<void(const std::string&)>& f_hook, const std::vector<std::string>& f_targetBlocks)
+    void BlockProfiler::printImpl(const double f_divisor, const char *const f_unit, const std::function<void(const std::string &)> &f_hook, const std::vector<std::string> &f_targetBlocks)
     {
-        std::lock_guard<std::mutex> l_lock{getMapMutex()};
-        auto& l_instances = getInstances();
+        std::lock_guard<std::mutex> l_lock{s_mapMutex};
+        auto &l_instances = getInstances();
 
         if (l_instances.empty())
         {
@@ -77,7 +107,7 @@ namespace prf
         if (!f_targetBlocks.empty())
         {
             bool l_anyFound = false;
-            for (const auto& l_target : f_targetBlocks)
+            for (const auto &l_target : f_targetBlocks)
             {
                 if (l_instances.find(l_target) != l_instances.end())
                 {
@@ -97,13 +127,14 @@ namespace prf
         f_hook("=========================================\n");
 
         std::ostringstream l_oss;
-        auto l_flushLine = [&l_oss, &f_hook]() {
+        auto l_flushLine = [&l_oss, &f_hook]()
+        {
             f_hook(l_oss.str());
             l_oss.str("");
             l_oss.clear();
         };
 
-        for (const auto& l_blockPair : l_instances)
+        for (const auto &l_blockPair : l_instances)
         {
             if (!f_targetBlocks.empty() && std::find(f_targetBlocks.begin(), f_targetBlocks.end(), l_blockPair.first) == f_targetBlocks.end())
             {
@@ -112,22 +143,22 @@ namespace prf
 
             l_oss << "Block: [" << l_blockPair.first << "]\n";
             l_flushLine();
-            BlockProfiler* l_profiler = l_blockPair.second.get();
+            BlockProfiler *l_profiler = l_blockPair.second.get();
 
             std::lock_guard<std::mutex> l_blockLock{l_profiler->m_mutex};
-            
+
             std::string l_minCol = std::string("Min (") + f_unit + ")";
             std::string l_maxCol = std::string("Max (") + f_unit + ")";
             std::string l_avgCol = std::string("Avg (") + f_unit + ")";
             std::string l_totalCol = std::string("Total (") + f_unit + ")";
 
-            l_oss << std::left 
-                << std::setw(25) << "Instance" << " | "
-                << std::setw(8)  << "Count" << " | "
-                << std::setw(12) << l_minCol << " | "
-                << std::setw(12) << l_maxCol << " | "
-                << std::setw(12) << l_avgCol << " | "
-                << std::setw(15) << l_totalCol << " |\n";
+            l_oss << std::left
+                  << std::setw(25) << "Instance" << " | "
+                  << std::setw(8) << "Count" << " | "
+                  << std::setw(12) << l_minCol << " | "
+                  << std::setw(12) << l_maxCol << " | "
+                  << std::setw(12) << l_avgCol << " | "
+                  << std::setw(15) << l_totalCol << " |\n";
             l_flushLine();
 
             if (f_divisor != 1.0)
@@ -135,26 +166,26 @@ namespace prf
                 l_oss << std::fixed << std::setprecision(3);
             }
 
-            for (const auto& l_scopePair : l_profiler->m_scopesDataMap)
+            for (const auto &l_scopePair : l_profiler->m_scopesDataMap)
             {
-                const ScopeData& l_data = l_scopePair.second;
-                l_oss << std::left 
-                    << std::setw(25) << l_scopePair.first << " | "
-                    << std::setw(8)  << l_data.m_count << " | ";
-                
+                const ScopeData &l_data = l_scopePair.second;
+                l_oss << std::left
+                      << std::setw(25) << l_scopePair.first << " | "
+                      << std::setw(8) << l_data.m_count << " | ";
+
                 if (f_divisor == 1.0)
                 {
                     l_oss << std::setw(12) << l_data.m_minTimeNs << " | "
-                        << std::setw(12) << l_data.m_maxTimeNs << " | "
-                        << std::setw(12) << l_data.m_avgTimeNs << " | "
-                        << std::setw(15) << l_data.m_totalTimeNs << " |\n";
+                          << std::setw(12) << l_data.m_maxTimeNs << " | "
+                          << std::setw(12) << l_data.m_avgTimeNs << " | "
+                          << std::setw(15) << l_data.m_totalTimeNs << " |\n";
                 }
                 else
                 {
                     l_oss << std::setw(12) << (l_data.m_minTimeNs / f_divisor) << " | "
-                        << std::setw(12) << (l_data.m_maxTimeNs / f_divisor) << " | "
-                        << std::setw(12) << (l_data.m_avgTimeNs / f_divisor) << " | "
-                        << std::setw(15) << (l_data.m_totalTimeNs / f_divisor) << " |\n";
+                          << std::setw(12) << (l_data.m_maxTimeNs / f_divisor) << " | "
+                          << std::setw(12) << (l_data.m_avgTimeNs / f_divisor) << " | "
+                          << std::setw(15) << (l_data.m_totalTimeNs / f_divisor) << " |\n";
                 }
                 l_flushLine();
             }
@@ -164,10 +195,10 @@ namespace prf
     }
 
     template <typename StringType>
-    BlockProfiler& BlockProfiler::getInstance(const StringType& f_blockName)
+    BlockProfiler &BlockProfiler::getInstance(const StringType &f_blockName)
     {
-        std::lock_guard<std::mutex> l_lock{getMapMutex()};
-        auto& l_instances = getInstances();
+        std::lock_guard<std::mutex> l_lock{s_mapMutex};
+        auto &l_instances = getInstances();
         auto l_it = l_instances.find(f_blockName);
         if (l_it == l_instances.end())
         {
@@ -179,11 +210,11 @@ namespace prf
     }
 
     // Explicit instantiations for getInstance
-    template BlockProfiler& BlockProfiler::getInstance<const char*>(const char* const&);
-    template BlockProfiler& BlockProfiler::getInstance<std::string>(const std::string&);
+    template BlockProfiler &BlockProfiler::getInstance<const char *>(const char *const &);
+    template BlockProfiler &BlockProfiler::getInstance<std::string>(const std::string &);
 
     template <typename StringType>
-    void BlockProfiler::add(const StringType& f_scopeName, const uint64_t f_timeTaken)
+    void BlockProfiler::add(const StringType &f_scopeName, const uint64_t f_timeTaken)
     {
         std::lock_guard<std::mutex> l_lock{m_mutex};
         auto l_it = m_scopesDataMap.find(f_scopeName);
@@ -191,8 +222,8 @@ namespace prf
         {
             l_it = m_scopesDataMap.emplace(std::string{f_scopeName}, ScopeData{}).first;
         }
-        
-        auto& l_scopeData = l_it->second;
+
+        auto &l_scopeData = l_it->second;
         l_scopeData.m_count++;
         l_scopeData.m_totalTimeNs += f_timeTaken;
         l_scopeData.m_minTimeNs = std::min(l_scopeData.m_minTimeNs, f_timeTaken);
@@ -201,12 +232,11 @@ namespace prf
     }
 
     // Explicit instantiations for add
-    template void BlockProfiler::add<const char*>(const char* const&, const uint64_t);
-    template void BlockProfiler::add<std::string>(const std::string&, const uint64_t);
+    template void BlockProfiler::add<const char *>(const char *const &, const uint64_t);
+    template void BlockProfiler::add<std::string>(const std::string &, const uint64_t);
 
-    ScopeProfile::ScopeProfile(const char* const f_blockName, const char* const f_scopeName) :
-        m_profiler(BlockProfiler::getInstance(f_blockName)), // Cache reference to avoid map lookup on destruction
-        m_scopeName(f_scopeName)
+    ScopeProfile::ScopeProfile(const char *const f_blockName, const char *const f_scopeName) : m_profiler(BlockProfiler::getInstance(f_blockName)), // Cache reference to avoid map lookup on destruction
+                                                                                               m_scopeName(f_scopeName)
     {
         if (BlockProfiler::getProfileEnabled().load(std::memory_order_relaxed))
         {
--- main.cpp
+++ main.cpp
@@ -5,13 +5,16 @@
 #include <utility>
 #include "Profiler.hpp"
 
+static const char* mainThread = "MainThread";
+static const char* workerTasks = "WorkerTasks";
+
 void workerTask(const int f_sleepMs)
 {
-    prf::ScopeProfile l_profile("WorkerTasks", "Thread Sleep");
+    prf::ScopeProfile l_profile(workerTasks, "Thread Sleep");
     std::this_thread::sleep_for(std::chrono::milliseconds(f_sleepMs));
 
     {
-        prf::ScopeProfile l_subProfile("WorkerTasks", "Inner Loop");
+        prf::ScopeProfile l_subProfile(workerTasks, "Inner Loop");
         for (int l_i = 0; l_i < 3; ++l_i)
         {
             std::this_thread::sleep_for(std::chrono::milliseconds(15));
@@ -21,7 +24,7 @@ void workerTask(const int f_sleepMs)
 
 void dbQuery()
 {
-    prf::ScopeProfile l_profile("MainThread", "Database Query");
+    prf::ScopeProfile l_profile(mainThread, "Database Query");
     std::this_thread::sleep_for(std::chrono::milliseconds(120));
 }
 
@@ -49,7 +52,7 @@ int main()
     std::cout << "Starting profiled tasks...\n";
 
     {
-        prf::ScopeProfile l_mainProfile("MainThread", "Total Execution");
+        prf::ScopeProfile l_mainProfile(mainThread, "Total Execution");
         
         dbQuery();
 
@@ -64,7 +67,7 @@ int main()
     // Print recorded profiling data
     // This uses the custom hook we provided to profilerInit()
     prf::BlkProf::printAll(prf::Precision::Micro);
-    std::vector<std::string> l_vecBlock{"WorkerTasks", "MainThread"};
+    std::vector<std::string> l_vecBlock{workerTasks, mainThread};
     prf::BlkProf::print(prf::Precision::Milli, l_vecBlock);
 
     return 0;
